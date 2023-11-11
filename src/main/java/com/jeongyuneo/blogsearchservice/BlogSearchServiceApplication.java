6be408d80e42308bd098a8a2302617f596125f6e
jeongyuneo <piq2255@gmail.com>
2023-11-11 16:56:41 +0900

    chore: 프로젝트 세팅

---
+++ src/main/java/com/jeongyuneo/blogsearchservice/BlogSearchServiceApplication.java
@@ -0,0 +1,12 @@
+package com.jeongyuneo.blogsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/jeongyuneo/blogsearchservice/BlogSearchServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.jeongyuneo.blogsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
