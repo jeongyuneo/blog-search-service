169a1b9c1c4b77f84ea684814f36bcc020859414
jeongyuneo <piq2255@gmail.com>
2023-11-12 23:29:20 +0900

    feat: ApplicationExceptionHandler 요청 파라미터 검증 예외 핸들러 추가

--- src/main/java/com/jeongyuneo/blogsearchservice/global/handler/ApplicationExceptionHandler.java
+++ src/main/java/com/jeongyuneo/blogsearchservice/global/handler/ApplicationExceptionHandler.java
@@ -2,10 +2,14 @@ package com.jeongyuneo.blogsearchservice.global.handler;
 
 import com.jeongyuneo.blogsearchservice.global.dto.ErrorResponse;
 import com.jeongyuneo.blogsearchservice.global.exception.ApplicationException;
+import com.jeongyuneo.blogsearchservice.global.exception.ApplicationExceptionInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import javax.validation.ConstraintViolationException;
 
 @Slf4j
 @RestControllerAdvice
@@ -17,5 +21,13 @@ public class ApplicationExceptionHandler {
         return ResponseEntity
                 .status(exception.getStatus())
                 .body(ErrorResponse.from(exception));
+    }
+
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, ConstraintViolationException.class})
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(Exception exception) {
+        log.info("{}: {}", exception.getClass().getSimpleName(), exception.getMessage(), exception);
+        return ResponseEntity
+                .badRequest()
+                .body(ErrorResponse.from(ApplicationExceptionInfo.INVALID_REQUEST_PARAMETER.getCode(), exception.getMessage()));
     }
 }
