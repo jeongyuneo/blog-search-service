18a210226b576e9e4dd983ec8d48c3e6d7115f2f
jeongyuneo <piq2255@gmail.com>
2023-11-12 23:26:19 +0900

    feat: ApplicationExceptionInfo 카카오 API 요청 실패 예외 정보 추가

--- src/main/java/com/jeongyuneo/blogsearchservice/global/exception/ApplicationExceptionInfo.java
+++ src/main/java/com/jeongyuneo/blogsearchservice/global/exception/ApplicationExceptionInfo.java
@@ -10,6 +10,8 @@ public enum ApplicationExceptionInfo {
 
     INVALID_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "WR-001", "잘못된 요청 파라미터가 포함되어 있습니다."),
 
+    KAKAO_API_REQUEST_FAIL(HttpStatus.BAD_REQUEST, "RF-001", "카카오 API 요청에 실패했습니다."),
+
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "IS-001", "서버 내부 에러가 발생했습니다.");
 
     private final HttpStatus status;
