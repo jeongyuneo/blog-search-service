4b3efdedef04fcec7c2f850f9216c351d0bbf622
jeongyuneo <piq2255@gmail.com>
2023-11-12 23:23:08 +0900

    feat: KakaoBlogSearchService 카카오 블로그 키워드 검색 추가

---
+++ src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/service/KakaoBlogSearchService.java
@@ -0,0 +1,90 @@
+package com.jeongyuneo.blogsearchservice.blogsearch.service;
+
+import com.jeongyuneo.blogsearchservice.blogsearch.dto.BlogSearchResponse;
+import com.jeongyuneo.blogsearchservice.blogsearch.dto.BlogSearchServiceRequest;
+import com.jeongyuneo.blogsearchservice.blogsearch.dto.kakaoapi.Document;
+import com.jeongyuneo.blogsearchservice.blogsearch.dto.kakaoapi.KakaoBlogSearchResponse;
+import com.jeongyuneo.blogsearchservice.global.exception.ApplicationExceptionInfo;
+import com.jeongyuneo.blogsearchservice.global.exception.InvalidArgumentException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+import java.util.Objects;
+
+@RequiredArgsConstructor
+@Service
+public class KakaoBlogSearchService implements BlogSearchService {
+
+    private static final String KAKAO_BLOG_SEARCH_URL = "https://dapi.kakao.com/v2/search/blog";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String KAKAO_AUTHORIZATION_HEADER_PREFIX = "KakaoAK ";
+    private static final String BLOG_SEARCH_URL = "http://localhost:8080/api/v1/blogs/search";
+    private static final String QUERY_PARAMETER_NAME = "query";
+    private static final String SORT_PARAMETER_NAME = "sort";
+    private static final String PAGE_PARAMETER_NAME = "page";
+
+    private final RestTemplate restTemplate;
+
+    @Value("${kakao.api.key}")
+    private String API_KEY;
+
+    @Override
+    public BlogSearchResponse searchByQuery(BlogSearchServiceRequest request) {
+        KakaoBlogSearchResponse kakaoBlogSearchResponse = requestBookSearch(getRequestUrl(request), new HttpEntity<>(getHeaders()));
+        List<Document> documents = Objects.requireNonNull(kakaoBlogSearchResponse).getDocuments();
+        if (documents.isEmpty()) {
+            return BlogSearchResponse.empty();
+        }
+        if (hasNoNext(request, kakaoBlogSearchResponse)) {
+            return BlogSearchResponse.from(documents);
+        }
+        return BlogSearchResponse.of(documents, getNextUrl(request));
+    }
+
+    private String getRequestUrl(BlogSearchServiceRequest request) {
+        return getUrl(KAKAO_BLOG_SEARCH_URL, request.getQuery(), request.getSort(), request.getPage());
+    }
+
+    private HttpHeaders getHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add(AUTHORIZATION_HEADER, KAKAO_AUTHORIZATION_HEADER_PREFIX + API_KEY);
+        return headers;
+    }
+
+    private KakaoBlogSearchResponse requestBookSearch(String requestUri, HttpEntity<HttpHeaders> httpEntity) {
+        try {
+            return restTemplate.exchange(requestUri, HttpMethod.GET, httpEntity, KakaoBlogSearchResponse.class)
+                    .getBody();
+        } catch (HttpClientErrorException exception) {
+            exception.printStackTrace();
+            throw new InvalidArgumentException(ApplicationExceptionInfo.KAKAO_API_REQUEST_FAIL);
+        }
+    }
+
+    private boolean hasNoNext(BlogSearchServiceRequest request, KakaoBlogSearchResponse kakaoBlogSearchResponse) {
+        return request.isMaxPage() || kakaoBlogSearchResponse.getMeta().getIs_end();
+    }
+
+    private String getNextUrl(BlogSearchServiceRequest request) {
+        return getUrl(BLOG_SEARCH_URL, request.getQuery(), request.getSort(), request.getPage() + 1);
+    }
+
+    private String getUrl(String baseUrl, String query, String sort, int page) {
+        return UriComponentsBuilder.fromUriString(baseUrl)
+                .queryParam(QUERY_PARAMETER_NAME, query)
+                .queryParam(SORT_PARAMETER_NAME, sort)
+                .queryParam(PAGE_PARAMETER_NAME, page)
+                .build()
+                .toString();
+    }
+}
