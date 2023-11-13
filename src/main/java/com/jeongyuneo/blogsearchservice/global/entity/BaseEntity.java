44fcaab0e91883397163f3b7ad931b95ece26015
jeongyuneo <piq2255@gmail.com>
2023-11-14 01:22:31 +0900

    feat: BaseEntity 추가

---
+++ src/main/java/com/jeongyuneo/blogsearchservice/global/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.jeongyuneo.blogsearchservice.global.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    protected LocalDateTime createdAt;
+
+    @LastModifiedDate
+    protected LocalDateTime lastModifiedAt;
+
+    protected boolean deleted;
+}
