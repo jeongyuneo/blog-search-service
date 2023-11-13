fac49364e630199511b879d737db17448cbf5c3a
jeongyuneo <piq2255@gmail.com>
2023-11-14 01:27:42 +0900

    feat: BlogSearchRankingService 인기 검색어 조회 추가

--- src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/service/BlogSearchRankingService.java
+++ src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/service/BlogSearchRankingService.java
@@ -1,9 +1,14 @@
 package com.jeongyuneo.blogsearchservice.blogsearch.service;
 
+import com.jeongyuneo.blogsearchservice.blogsearch.dto.BlogSearchRankingResponse;
+import com.jeongyuneo.blogsearchservice.blogsearch.dto.BlogSearchRankingResponseElement;
 import com.jeongyuneo.blogsearchservice.blogsearch.entity.BlogSearch;
 import com.jeongyuneo.blogsearchservice.blogsearch.repository.BlogSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -17,5 +22,13 @@ public class BlogSearchRankingService {
                         BlogSearch::increase,
                         () -> blogSearchRepository.save(BlogSearch.from(keyword))
                 );
+    }
+
+    public BlogSearchRankingResponse getSearchRanking() {
+        List<BlogSearchRankingResponseElement> responseElements = blogSearchRepository.findTop10ByOrderByCountDesc()
+                .stream()
+                .map(BlogSearchRankingResponseElement::from)
+                .collect(Collectors.toList());
+        return BlogSearchRankingResponse.from(responseElements);
     }
 }
