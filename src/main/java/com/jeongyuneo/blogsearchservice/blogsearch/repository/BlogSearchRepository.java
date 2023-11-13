18373e834c7fb87e65bb8827f6e182325ee17b2f
jeongyuneo <piq2255@gmail.com>
2023-11-14 01:28:30 +0900

    feat: BlogSearchRepository count 내림차순 정렬 상위 10개 조회 메소드 추가

--- src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/repository/BlogSearchRepository.java
+++ src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/repository/BlogSearchRepository.java
@@ -3,9 +3,12 @@ package com.jeongyuneo.blogsearchservice.blogsearch.repository;
 import com.jeongyuneo.blogsearchservice.blogsearch.entity.BlogSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BlogSearchRepository extends JpaRepository<BlogSearch, Long> {
 
     Optional<BlogSearch> findByKeyword(String keyword);
+
+    List<BlogSearch> findTop10ByOrderByCountDesc();
 }
