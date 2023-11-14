1f260ffa09764710876d5c49b0d9475f6b82a47c
jeongyuneo <piq2255@gmail.com>
2023-11-14 14:32:36 +0900

    test: BlogSearchRankingServiceTest 인기 검색어 목록 조회 시 검색기록이 10개 이하일 경우 테스트 추가

--- src/test/java/com/jeongyuneo/blogsearchservice/blogsearch/service/BlogSearchRankingServiceTest.java
+++ src/test/java/com/jeongyuneo/blogsearchservice/blogsearch/service/BlogSearchRankingServiceTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("검색 랭킹 서비스 테스트")
@@ -17,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class BlogSearchRankingServiceTest {
 
     private static final int INITIAL_BLOG_SEARCH_COUNT = 1;
+    private static final int MAX_BLOG_SEARCH_RANKING_COUNT = 10;
 
     @Autowired
     private BlogSearchRankingService blogSearchRankingService;
@@ -66,6 +69,21 @@ class BlogSearchRankingServiceTest {
             BlogSearchRankingResponse searchRanking = blogSearchRankingService.getSearchRanking();
             // then
             assertThat(searchRanking.getBlogSearchRankings()).isEmpty();
+        }
+
+        @Test
+        void 검색기록이_10개이하면_모든_검색기록을_반환한다() {
+            // given
+            List<BlogSearch> blogSearches = List.of(
+                    BlogSearch.from("키워드1"),
+                    BlogSearch.from("키워드2"),
+                    BlogSearch.from("키워드3")
+            );
+            blogSearchRepository.saveAll(blogSearches);
+            // when
+            BlogSearchRankingResponse searchRanking = blogSearchRankingService.getSearchRanking();
+            // then
+            assertThat(searchRanking.getBlogSearchRankings()).hasSizeLessThan(MAX_BLOG_SEARCH_RANKING_COUNT);
         }
     }
 }
