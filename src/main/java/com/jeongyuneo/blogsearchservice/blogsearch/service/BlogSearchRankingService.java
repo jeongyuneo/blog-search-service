ac6fbcf6f312871f2de5d73d2bd6cf172ba4bb5d
jeongyuneo <piq2255@gmail.com>
2023-11-15 21:03:07 +0900

    refactor: BlogSearchRankingService Transactional 어노테이션 추가

--- src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/service/BlogSearchRankingService.java
+++ src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/service/BlogSearchRankingService.java
@@ -6,6 +6,7 @@ import com.jeongyuneo.blogsearchservice.blogsearch.entity.BlogSearch;
 import com.jeongyuneo.blogsearchservice.blogsearch.repository.BlogSearchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,6 +17,7 @@ public class BlogSearchRankingService {
 
     private final BlogSearchRepository blogSearchRepository;
 
+    @Transactional
     public void increaseSearchCount(String keyword) {
         BlogSearch blogSearch = blogSearchRepository.findByKeyword(keyword);
         if (blogSearch == null) {
@@ -25,6 +27,7 @@ public class BlogSearchRankingService {
         blogSearch.increase();
     }
 
+    @Transactional(readOnly = true)
     public BlogSearchRankingResponse getSearchRanking() {
         List<BlogSearchRankingResponseElement> responseElements = blogSearchRepository.findTop10ByOrderByCountDesc()
                 .stream()
