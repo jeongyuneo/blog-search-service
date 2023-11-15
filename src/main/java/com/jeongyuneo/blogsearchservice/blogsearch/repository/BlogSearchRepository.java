9651943f0dddae4422c9b9f31ffad8f39c481d87
jeongyuneo <piq2255@gmail.com>
2023-11-15 13:00:53 +0900

    refactor: BlogSearchRepository 키워드로 검색기록 조회 반환타입 Optional 삭제

--- src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/repository/BlogSearchRepository.java
+++ src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/repository/BlogSearchRepository.java
@@ -4,11 +4,10 @@ import com.jeongyuneo.blogsearchservice.blogsearch.entity.BlogSearch;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BlogSearchRepository extends JpaRepository<BlogSearch, Long> {
 
-    Optional<BlogSearch> findByKeyword(String keyword);
+    BlogSearch findByKeyword(String keyword);
 
     List<BlogSearch> findTop10ByOrderByCountDesc();
 }
