d0119fc1f3540ec59c956ce8071c8df3416b0552
jeongyuneo <piq2255@gmail.com>
2023-12-05 19:52:27 +0900

    feat: BlogSearchRankingResponseElement 생성 메소드 네이밍 및 파라미터 수정

--- src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/dto/BlogSearchRankingResponseElement.java
+++ src/main/java/com/jeongyuneo/blogsearchservice/blogsearch/dto/BlogSearchRankingResponseElement.java
@@ -1,6 +1,5 @@
 package com.jeongyuneo.blogsearchservice.blogsearch.dto;
 
-import com.jeongyuneo.blogsearchservice.blogsearch.entity.BlogSearch;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,7 +11,7 @@ public class BlogSearchRankingResponseElement {
     private String keyword;
     private int count;
 
-    public static BlogSearchRankingResponseElement from(BlogSearch blogSearch) {
-        return new BlogSearchRankingResponseElement(blogSearch.getKeyword(), blogSearch.getCount());
+    public static BlogSearchRankingResponseElement of(String keyword, Double count) {
+        return new BlogSearchRankingResponseElement(keyword, count.intValue());
     }
 }
