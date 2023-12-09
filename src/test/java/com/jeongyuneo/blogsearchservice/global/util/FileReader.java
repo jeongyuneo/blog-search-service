9c8a101a133aedf2b9d8a25b21f570b8cab0cea1
jeongyuneo <piq2255@gmail.com>
2023-12-09 19:50:21 +0900

    test: FileReader 수정

--- src/test/java/com/jeongyuneo/blogsearchservice/global/util/FileReader.java
+++ src/test/java/com/jeongyuneo/blogsearchservice/global/util/FileReader.java
@@ -6,13 +6,11 @@ import java.nio.file.Paths;
 
 public class FileReader {
 
-    private static final String JSON_FILE_PATH = "src/test/java/resources/kakaoapi/";
-
-    public FileReader() {
+    private FileReader() {
         throw new UnsupportedOperationException();
     }
 
-    public static String readJson(String fileName) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(JSON_FILE_PATH + fileName)));
+    public static String readJson(String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
     }
 }
